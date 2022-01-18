4ef754fc25b48dbce693d945eb7b5c002f67f5aa
Daniel Rozmajzl <daniel.rozmajzl@gmail.com>
2022-01-18 18:11:44 -0500

    commit tree 9255f28433a93c34dfa7cb3e97209decabf38f06

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name, email){
+    const configurationObject = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json",
+            },
+            body: JSON.stringify({
+                name: `${name}`,
+                email: `${email}`,
+            })
+          }
+         return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      let bodyElement = document.querySelector('body')
+      bodyElement.append(`<p>${object.id}</p>`)
+      console.log(`${object}`)
+    })
+    .catch(function (error) {
+        let bodyElement2 = document.querySelector('body')
+        bodyElement2.append(`<p>${error.message}}</p>`)
+        console.log(error.message);
+    })
+}
